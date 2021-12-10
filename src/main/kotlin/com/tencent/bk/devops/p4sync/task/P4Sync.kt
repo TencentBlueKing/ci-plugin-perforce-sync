c414e5e0d7032a72ed05b7bbdf18ab5e413439be
felixncheng <felixncheng@tencent.com>
2021-12-10 12:27:29 +0800

    fix: 删除配置文件只读。Clobber默认为true

--- src/main/kotlin/com/tencent/bk/devops/p4sync/task/P4Sync.kt
+++ src/main/kotlin/com/tencent/bk/devops/p4sync/task/P4Sync.kt
@@ -173,6 +173,5 @@ class P4Sync : TaskAtom<P4SyncParam> {
             printWriter.print("$P4_CLIENT=${client.name}")
             logger.info("保存工作空间配置文件${configFilePath.toFile().canonicalPath}成功")
         }
-        configFilePath.toFile().setReadOnly()
     }
 }
